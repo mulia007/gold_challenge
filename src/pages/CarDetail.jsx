8cbf390017621c5d84155bdaaa2da2ae37486a67
mulia007 <116814230+mulia007@users.noreply.github.com>
2022-12-05 22:04:51 +0700

    Update CarDetail.jsx

--- src/pages/CarDetail.jsx
+++ src/pages/CarDetail.jsx
@@ -33,79 +33,92 @@ const CarDetail = () => {
       <br />
       <br />
       <Filter disabled={true} isButton={false} />
-      <div className="container" style={{ marginTop: "70px" }}>
-        <div className="col-lg-6">
-          <Col lg={7}>
-            <Card className="p-5 mb-6">
-              {Object.entries(car).length ? (
-                <div>
-                  <img
-                    src={car.image}
-                    width={270}
-                    height={160}
-                    top={396}
-                    left={904}
-                    style={{
-                      marginBottom: "40px",
-                      marginTop: "40px",
-                      marginLeft: "40px",
-                      alignItems: "center",
-                    }}
-                  />
-                  <h1>{car.name}</h1>
+      <section class="detail">
+        <div class="container py-lg-5">
+          <div class="row">
+            <div class="col">
+              <div class="card">
+                <div class="card-body">
+                  <h6>Tentang Paket</h6>
+                  <h6>Include</h6>
+                  <ul class="text-muted">
+                    <li>Apa saja yang termasuk dalam paket misal durasi max 12 jam</li>
+                    <li>Sudah termasuk bensin selama 12 jam</li>
+                    <li>Sudah termasuk Tiket Wisata</li>
+                    <li>Sudah termasuk pajak</li>
+                  </ul>
 
-                  <p>
-                    <img src="../assets/users.png" />
-                    {car.category === "Medium" && "4 - 6 Orang"}
-                    {car.category === "small" && "2 - 4 Orang"}
-                    {car.category === "medium" && "4 - 6 Orang"}
-                    {car.category === "large" && "6 - 8 Orang"}
-                  </p>
-                  <p>Total : Rp. {car.price}</p>
+                  <h6>Exclude</h6>
+                  <ul class="text-muted">
+                    <li>Tidak termasuk biaya makan sopir Rp 75.000/hari </li>
+                    <li>Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam</li>
+                    <li>Tidak termasuk akomodasi penginapan</li>
+                  </ul>
+
+                  <h6>Exclude</h6>
+                  <ul class="text-muted">
+                    <li>Tidak termasuk biaya makan sopir Rp 75.000/hari </li>
+                    <li>Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam</li>
+                    <li>Tidak termasuk akomodasi penginapan</li>
+                    <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
+                    <li>Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam</li>
+                    <li>Tidak termasuk akomodasi penginapan</li>
+                    <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
+                    <li>Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam</li>
+                    <li>Tidak termasuk akomodasi penginapan</li>
+                  </ul>
                 </div>
-              ) : (
-                <h1></h1>
-              )}
-            </Card>
-          </Col>
-        </div>
-        <div className="row">
-          <div>
-            <Col lg={7}>
-              <Card className="p-5 mb-6">
-                <h1 className="mb-5 fw-bold">Tentang Paket</h1>
-                <h2 className="fw-bold">Include</h2>
-                <ul>
-                  <li>Apa saja yang termasuk dalam paket misal durasi max 12 jam</li>
-                  <li>Sudah termasuk bensin selama 12 jam</li>
-                  <li>Sudah termasuk Tiket Wisata</li>
-                  <li>Sudah termasuk pajak</li>
-                </ul>
-                <h2 className="fw-bold">Exclude</h2>
-                <ul>
-                  <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
-                  <li>Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam</li>
-                  <li>Tidak termasuk akomodasi penginapan</li>
-                </ul>
-                <h2 className="fw-bold">Refund, Reschedule, Overtime</h2>
-                <ul>
-                  <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
-                  <li>Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam</li>
-                  <li>Tidak termasuk akomodasi penginapan</li>
-                  <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
-                  <li>Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam</li>
-                  <li>Tidak termasuk akomodasi penginapan</li>
-                  <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
-                  <li>Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam</li>
-                  <li>Tidak termasuk akomodasi penginapan</li>
-                </ul>
-              </Card>
-            </Col>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card">
+                <div class="card-body">
+                  {Object.entries(car).length ? (
+                    <div>
+                      <img
+                        src={car.image}
+                        width={270}
+                        height={160}
+                        top={396}
+                        left={904}
+                        style={{
+                          marginBottom: "40px",
+                          marginTop: "40px",
+                          marginLeft: "40px",
+                          alignItems: "center",
+                        }}
+                      />
+                      <p>
+                        <img src="../assets/users.png" />
+                        {car.category === "Medium" && "4 - 6 Orang"}
+                        {car.category === "small" && "2 - 4 Orang"}
+                        {car.category === "medium" && "4 - 6 Orang"}
+                        {car.category === "large" && "6 - 8 Orang"}
+                      </p>
+                    </div>
+                  ) : (
+                    <h1></h1>
+                  )}
+                </div>
+                <div class="">
+                  <h6>{car.name}</h6>
+                  <i class=""></i>
+                </div>
+                <div class="row">
+                  <div class="col px-lg-5 py-lg-5">
+                    <h6 class="fw-bold">Total</h6>
+                  </div>
+                  <div class="col pe-5 py-lg-5 text-lg-end">
+                    <h6 class="fw-bold">Rp. {car.price}</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-
-      <Footer />
+      </section>
+      <Footer />;
     </div>
   );
 };
