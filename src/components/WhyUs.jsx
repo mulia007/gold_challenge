1c8a976ead8e641f82c0d8bbe4accc9f55f87828
mulia007 <116814230+mulia007@users.noreply.github.com>
2023-01-09 00:01:34 +0700

    Update WhyUs.jsx

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -14,7 +14,10 @@ const WhyUs = () => {
               <div className="card-body">
                 <img src="img/thumb.png" width={30} height={30} />
                 <h5 className="card-title">Mobil Lengkap</h5>
-                Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat
+                Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat. Lorem ipsum
+                dolor sit amet, consectetur adipisicing elit. Inventore commodi non cupiditate qui
+                quia pariatur quos rem totam eveniet, recusandae quam sint nam voluptatum excepturi
+                ea ut, autem laudantium vel.
               </div>
             </div>
           </div>
@@ -23,7 +26,10 @@ const WhyUs = () => {
               <div className="card-body">
                 <img src="img/tag.png" width={30} height={30} />
                 <h5 className="card-title">Harga Murah</h5>
-                Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain
+                Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain Lorem
+                ipsum dolor sit, amet consectetur adipisicing elit. Quaerat voluptatem magni fugit
+                praesentium excepturi, impedit libero minima delectus est ut aliquam. Quasi fugiat,
+                magni minima nisi dolorem debitis molestias! Itaque!
               </div>
             </div>
           </div>
@@ -33,7 +39,25 @@ const WhyUs = () => {
                 <img src="img/clock.png" width={30} height={30} />
                 <h5 className="card-title">Layanan 24 Jam</h5>
                 Siap melayani kebutuhan Anda selama 24 jam nonstop. kami juga tersedia di hari
-                weekend dan hari minggu
+                weekend dan hari minggu. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati quam hic quo inventore nostrum, dignissimos atque, quos assumenda eaque
+                fugiat doloremque cupiditate modi quis voluptatem accusantium doloribus voluptas,
+                laboriosam ullam. Architecto, recusandae aut ratione ab non, omnis quia saepe
+                tempore adipisci assumenda expedita enim sint atque possimus inventore modi nemo
+                dolor, consectetur quasi exercitationem? Eius veniam, ipsa repellendus sequi facilis
+                ratione quo dolorum sapiente nobis tempore repellat nulla rem, et, optio maiores hic
+                reiciendis molestias eveniet sint? Repudiandae at provident officiis quia
+                voluptatum, possimus a rerum incidunt porro corrupti, quibusdam doloremque iste
+                ratione quisquam dolor sunt obcaecati numquam, modi unde consectetur reprehenderit.
+                Quam dignissimos eos hic quisquam. Officia aperiam, adipisci nulla, maxime veritatis
+                at ad porro, modi numquam ea odit ipsa soluta quaerat nisi distinctio repellat neque
+                culpa quis! Quasi enim inventore assumenda veritatis vitae cum fugiat quaerat? Odit
+                cumque sunt natus voluptas, eaque veritatis impedit eos labore sed dignissimos in
+                recusandae officia iusto nihil explicabo ipsa harum soluta asperiores eveniet
+                incidunt? Odio voluptate aut earum ullam illum, velit reprehenderit itaque labore
+                perspiciatis ab id molestias sit reiciendis veritatis impedit assumenda obcaecati
+                consectetur! Vel dolore, perferendis hic laudantium commodi quidem iusto optio earum
+                eveniet est minus. Facere ad nemo beatae.
               </div>
             </div>
           </div>
