9d90ef94a8d32370acbceddc2035a9fd91ee648d
mulia007 <116814230+mulia007@users.noreply.github.com>
2023-01-15 23:11:56 +0700

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -12,23 +12,26 @@ const Welcome = (props) => {
               <p className="lead">
                 Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan
                 harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi enim beatae excepturi
-                sunt sint neque nesciunt tempora odio vero, sit, minus reiciendis dolor. Iure
-                laboriosam ipsa tempora, ipsam aperiam ratione. Lorem ipsum, dolor sit amet
-                consectetur adipisicing elit. Reiciendis quo, eos optio aspernatur magni tempora
-                ipsum totam veritatis! Cumque deleniti nostrum qui. Eos, error! Totam eos error eum,
-                delectus non debitis odit possimus laborum deleniti ea nihil hic ducimus expedita
-                nisi eius dicta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sunt
-                perspiciatis molestias enim, veritatis exercitationem! Ducimus nisi eius, nesciunt
-                cumque quod possimus repellat hic veritatis doloremque iste maiores nam! Labore
-                assumenda officia fugiat nesciunt modi fugit, alias facilis tenetur suscipit vero
-                veritatis dolorum?
               </p>
               {props.isButtonShow ? (
                 <div>
                   <button type="button" className="btn btn-success">
                     <Link to="/carimobil" style={{ textDecoration: "none", color: "#fff" }}>
-                      Mulai Sewa Mobil
+                      Mulai Sewa Mobil. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Omnis, aliquid. Autem laborum quaerat ex accusamus iure quod dolorum adipisci
+                      unde!. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde porro
+                      corrupti voluptas quibusdam similique iure a? Voluptatibus quidem accusantium
+                      iste expedita minus dolorum, provident eligendi ipsam labore accusamus at
+                      aliquam modi praesentium atque rem quas adipisci possimus quam id esse facilis
+                      obcaecati velit! Error blanditiis obcaecati commodi aliquid laboriosam a minus
+                      incidunt sint consequuntur qui, asperiores, amet neque, optio minima excepturi
+                      officiis. Nam dolores ex quas magnam laboriosam aliquid quod dolor error
+                      suscipit? Magni vitae libero illo adipisci quaerat, tempora vel ullam
+                      repudiandae commodi cumque est, itaque quas laboriosam! Explicabo quidem
+                      commodi officia iure voluptates delectus hic iusto cumque debitis animi, rem
+                      voluptatem exercitationem aspernatur consequuntur ipsa laboriosam unde non
+                      molestiae, optio incidunt atque temporibus perferendis. Similique sint natus
+                      expedita.
                     </Link>
                   </button>
                 </div>
